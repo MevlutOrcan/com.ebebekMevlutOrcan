24aba0df5cb5af27400cfa6e64ea76dd28278b39
Mevlut Orcan <mevlutorcan@gmail.com>
2022-10-22 17:21:09 +0300

    mevlutorcan

--- src/ebebek/Operations.java
+++ src/ebebek/Operations.java
@@ -8,22 +8,30 @@ public class Operations {
         Scanner scan=new Scanner(System.in);
         System.out.println("\n************** WELCOME TO WORKER INFORMATION PRINT PROGRAMME **************\n");
 
-        System.out.println("Enter the Name Please");
-        String name = scan.nextLine();
+        String name = null;
+        double salary= 0;
+        int workHours= 0;
+        int hireYear= 0;
+        try {
+            System.out.println("Enter the Name and Click Enter Please");
+            name = scan.nextLine();
 
-        System.out.println("Enter the Salary Please");
-        double salary=scan.nextDouble();
+            System.out.println("Enter the Salary and Click Enter Please");
+            salary = scan.nextDouble();
 
-        System.out.println("Enter the Work Hours Please");
-        int workHours= scan.nextInt();
+            System.out.println("Enter the Work Hours and Click Enter Please");
+            workHours = scan.nextInt();
 
-        System.out.println("Enter the Hire Year Please");
-        int hireYear= scan.nextInt();
+            System.out.println("Enter the Hire Year and Click Enter Please");
+            hireYear = scan.nextInt();
+        } catch (Exception e) {
+            System.out.println("We Are Sorry\nIncorrect Entry!");
+        }
 
         POJO pojo=new POJO(name,salary,workHours,hireYear);
         System.out.println(pojo);
 
-        System.out.println("\nDo You Want To Continue?\nFor 'Yes' Enter 'Y' Please\nFor 'No' Enter 'N' Please");
+        System.out.println("\nDo You Want To Continue?\nFor 'Yes' Enter 'Y' and Click Enter Please\nFor 'No' Enter 'N' and Click Enter  Please");
         String choosing=scan.next().substring(0,1);
         if (choosing.equalsIgnoreCase("y")){
             operations();
